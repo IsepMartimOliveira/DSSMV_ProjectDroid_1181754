0b90bcdfdaf15cefbf5864c0618ae123d439f240
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-09-28 23:12:45 +0100

    Register Page layout done, added error if null

--- app/src/main/java/com/example/onlinesupertmarket/Network/Utils.java
+++ app/src/main/java/com/example/onlinesupertmarket/Network/Utils.java
@@ -2,5 +2,7 @@ package com.example.onlinesupertmarket.Network;
 
 public class Utils {
     String apiUrl="https://api.spoonacular.com/";
+    String api_key="?apiKey=8731e20fa447459cba57412c132ca440";
+    String userCreate="users/connect";
 
 }
--- app/src/main/java/com/example/onlinesupertmarket/registerPage.java
+++ app/src/main/java/com/example/onlinesupertmarket/registerPage.java
@@ -1,20 +1,36 @@
 package com.example.onlinesupertmarket;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+
 
 public class registerPage extends AppCompatActivity {
-EditText editText;
-    TextView registerTextView ;
+    TextView registerTextView;
+    EditText username;
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
-        registerTextView=findViewById(R.id.signIn);
+        registerTextView = findViewById(R.id.signIn);
+        username = findViewById(R.id.username);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        email = findViewById(R.id.email);
+
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,4 +41,56 @@ EditText editText;
         });
 
     }
+
+    public void onRegisterClick(View view) {
+        try {
+            isBlank();
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void isBlank() {
+        boolean hasError = false;
+
+        if (TextUtils.isEmpty(username.getText().toString())) {
+            setError(username, true);
+            hasError = true;
+        } else {
+            setError(username, false);
+        }
+
+        if (TextUtils.isEmpty(firstName.getText().toString())) {
+            setError(firstName, true);
+            hasError = true;
+        } else {
+            setError(firstName, false);
+        }
+
+        if (TextUtils.isEmpty(lastName.getText().toString())) {
+            setError(lastName, true);
+            hasError = true;
+        } else {
+            setError(lastName, false);
+        }
+
+        if (TextUtils.isEmpty(email.getText().toString())) {
+            setError(email, true);
+            hasError = true;
+        } else {
+            setError(email, false);
+        }
+
+        if (hasError) {
+            throw new IllegalArgumentException("One or more fields are empty");
+        }
+    }
+
+    private void setError(EditText editText, boolean isError) {
+        if (isError) {
+            editText.setBackgroundResource(R.drawable.edittext_error_background);
+            editText.setBackground(null);
+        }
+    }
 }
+
