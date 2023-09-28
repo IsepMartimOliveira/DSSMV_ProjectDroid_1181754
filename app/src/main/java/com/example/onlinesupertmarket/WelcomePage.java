16200184e533f27ddd6fa448349a6f8cedd98bea
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-09-28 21:32:03 +0100

    Register Page layout done

--- app/src/main/java/com/example/onlinesupertmarket/WelcomePage.java
+++ app/src/main/java/com/example/onlinesupertmarket/WelcomePage.java
@@ -3,22 +3,35 @@ package com.example.onlinesupertmarket;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomePage extends AppCompatActivity {
     Button button;
+    TextView registerTextView ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        /*button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start the new activity
                 Intent intent = new Intent(WelcomePage.this, registerPage.class);
+                startActivity(intent);
+            }
+        });*/
+        registerTextView=findViewById(R.id.registerText);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the new activity
+                Intent intent = new Intent(WelcomePage.this, registerPage.class); // Replace with your activity names
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/onlinesupertmarket/registerPage.java
+++ app/src/main/java/com/example/onlinesupertmarket/registerPage.java
@@ -1,17 +1,28 @@
 package com.example.onlinesupertmarket;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class registerPage extends AppCompatActivity {
 EditText editText;
+    TextView registerTextView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
-        String title="Username";
-        EditText et = (EditText) findViewById(R.id.username);
-        et.setText(et.getText() + title);
+        registerTextView=findViewById(R.id.signIn);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the new activity
+                Intent intent = new Intent(registerPage.this, WelcomePage.class); // Replace with your activity names
+                startActivity(intent);
+            }
+        });
+
     }
 }
