34f9cf97180691f31a3298169c9009dc5741807f
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-10-05 02:39:44 +0100

    Add documentation. Changes on registerPage and WelcomePage

---
+++ app/src/main/java/com/example/onlinesupertmarket/RecepiePage.java
@@ -0,0 +1,13 @@
+package com.example.onlinesupertmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecepiePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recepie_page);
+    }
+}
