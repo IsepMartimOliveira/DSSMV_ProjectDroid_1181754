8e706f68052d92c1ea1e88f116e65549e8b1cee6
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-11-07 01:19:03 +0000

    Update

--- app/src/main/java/com/example/onlinesupertmarket/Network/Utils.java
+++ app/src/main/java/com/example/onlinesupertmarket/Network/Utils.java
@@ -2,8 +2,9 @@ package com.example.onlinesupertmarket.Network;
 
 public class Utils {
     public static String apiUrl="https://api.spoonacular.com/";
-    public static String api_key="?apiKey=8d7e8f2c62fb434182d0bc9f11914e08";
+    public static String api_key="?apiKey=8731e20fa447459cba57412c132ca440";
     //8d7e8f2c62fb434182d0bc9f11914e08
+    //8731e20fa447459cba57412c132ca440
     public static String userCreate="users/connect";
     public static String recipe="recipes/complexSearch";
     public static String cuisine="&cuisine=";
